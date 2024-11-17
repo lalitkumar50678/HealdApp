61dc51f2505d82dda999898f3c087205b868d77c
Lalit kumar <lalitkumar50678@gmail.com>
2024-11-17 17:30:57 +0530

    Home Screen view modifed

--- src/screens/HomeScreen/HomeScreenView.tsx
+++ src/screens/HomeScreen/HomeScreenView.tsx
@@ -8,33 +8,48 @@ import { LocalizationEN } from "../../i18n";
 import { HomeScreenViewProps } from "./types";
 import { TextType } from "../../components/atoms/Text/Types";
 import { Colors, generateShadow } from "../../utils";
-import StepsView  from '../../components/molecules/StepsView/StepsView';
-import {TOTAL_TARGET_STEPS} from '../../constants';
+import StepsView from "../../components/molecules/StepsView/StepsView";
+import { TOTAL_TARGET_STEPS } from "../../constants";
 import CaloriesView from "../../components/molecules/CaloriesView/CaloriesView";
-import StepsGraphView from '../../components/molecules/StepsGraphView/StepsGraphView';
+import StepsGraphView from "../../components/molecules/StepsGraphView/StepsGraphView";
 
-const HomeScreenView: React.FC<HomeScreenViewProps> = ({ steps,isPlaying,onStartStop,fill,distanceTravel,list }) => {
+const HomeScreenView: React.FC<HomeScreenViewProps> = ({
+  steps,
+  isPlaying,
+  onStartStop,
+  fill,
+  distanceTravel,
+  list,
+}) => {
   return (
     <View style={styles.container}>
       <ToolBar title={LocalizationEN.HOME} />
-      <View style={[styles.stepsViewStyle,generateShadow()]}>
-      <View style={styles.circluarStyle}>
-        <CircularProgressBar
-          fill={fill}
-          tintColor={Colors.CIRCULAR_FILL}
-          backgroundColor={Colors.CIRCULAR_UNFILL}
-          width={25}
-        >
-          {(fill) => (
-            <StepsView stepsCount={steps} targetStepsCount={TOTAL_TARGET_STEPS}  isPlaying={isPlaying} onStartStop={onStartStop} />
-          )}
-        </CircularProgressBar>
+      <View style={[styles.stepsViewStyle, generateShadow()]}>
+        <View style={styles.circluarStyle}>
+          <CircularProgressBar
+            fill={fill}
+            tintColor={Colors.CIRCULAR_FILL}
+            backgroundColor={Colors.CIRCULAR_UNFILL}
+            width={25}
+          >
+            {() => (
+              <StepsView
+                stepsCount={steps}
+                targetStepsCount={TOTAL_TARGET_STEPS}
+                isPlaying={isPlaying}
+                onStartStop={onStartStop}
+              />
+            )}
+          </CircularProgressBar>
 
-        <Text>{steps}</Text>
+          <Text type={TextType.secondary}>{steps}</Text>
+        </View>
       </View>
-      </View>
-      <CaloriesView  calories={10} distance={parseFloat(distanceTravel.toFixed(2)) } />
-      <StepsGraphView list={list} /> 
+      <CaloriesView
+        calories={10}
+        distance={parseFloat(distanceTravel.toFixed(2))}
+      />
+      <StepsGraphView list={list} />
     </View>
   );
 };
